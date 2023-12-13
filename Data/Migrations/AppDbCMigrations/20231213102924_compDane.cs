8c0106df4255d570ab53b0aa4a837e062c90e44d
wiktoria.zygmunt <wiktoria.zygmunt@SF-11>
2023-12-13 11:45:43 +0100

    dwie migracje

---
+++ Data/Migrations/AppDbCMigrations/20231213102924_compDane.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Data.Migrations.AppDbCMigrations
+{
+    /// <inheritdoc />
+    public partial class compDane : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
